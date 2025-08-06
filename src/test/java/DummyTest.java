e8ad698434ef54fe8e179ada09644af4202cbf58
Ada Stroescu <ada.stroescu@gmail.com>
2025-08-06 13:39:37 +0000

    Junit

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,10 @@
+DummyTest.java with the following content:
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class DummyTest {
+@Test
+public void test() {
+fail("Test intentionaly failing");
+}
+}
+

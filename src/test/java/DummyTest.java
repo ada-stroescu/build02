f1633a2988bb623285e56f786d09fcafb3780861
Ada Stroescu <ada.stroescu@gmail.com>
2025-08-06 13:49:53 +0000

    Second test

--- src/test/java/DummyTest.java
+++ src/test/java/DummyTest.java
@@ -1,10 +1,9 @@
-DummyTest.java with the following content:
 import static org.junit.Assert.*;
 import org.junit.Test;
 public class DummyTest {
 @Test
 public void test() {
-fail("Test intentionaly failing");
+assertTrue(true);
 }
 }
 
